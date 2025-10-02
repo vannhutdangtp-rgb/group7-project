f118bd692be60ac509e44ecac48d979b9b363398
vannhutdangtp-rgb <nhut220999@student.nctu.edu>
2025-10-02 14:55:42 +0700

    Khởi tạo backend với Express

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 // server.js
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
-// Middleware để parse JSON
 app.use(express.json());
 
-// Biến PORT lấy từ file .env (nếu có), mặc định = 3000
 const PORT = process.env.PORT || 3000;
 
-// Khởi động server
+app.get('/', (req, res) => res.send('API is running'));
+
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
