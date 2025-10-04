50d530ee8ae1424f2b302635bc8a10a4bf577d58
HoangNguyenHuuLoc <loc210009@student.nctu.edu.vn>
2025-10-04 16:35:09 +0700

    LocSua

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
-
 const API = axios.create({
   baseURL: "http://localhost:5000",
 });
-
 export const getUsers = () => API.get("/users");
 export const addUser = (user) => API.post("/users", user);
 export const updateUser = (id, user) => API.put(`/users/${id}`, user);
 export const deleteUser = (id) => API.delete(`/users/${id}`);
+///LocDaSua
