afe01cf7adeb50564e497562f67a66b0f75e4084
vannhutdangtp-rgb <nhut220999@student.nctu.edu>
2025-10-09 14:15:18 +0700

    Khôi phục file api.js sau merge lỗi

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -6,9 +6,6 @@ export const getUsers = () => API.get("/users");
 export const addUser = (user) => API.post("/users", user);
 export const updateUser = (id, user) => API.put(`/users/${id}`, user);
 export const deleteUser = (id) => API.delete(`/users/${id}`);
-<<<<<<< HEAD
 //Nhutdasua
 ///LocDaSua
-=======
 ///LocDaSua
->>>>>>> frontend
