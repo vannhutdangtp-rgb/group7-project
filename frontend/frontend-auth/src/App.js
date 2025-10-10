4b900ba092e4c765c1b8ffb29711d66a8ea4cf89
vannhutdangtp-rgb <nhut220999@student.nctu.edu>
2025-10-10 20:31:55 +0700

    Update Activity 2 frontend-profile

--- frontend/frontend-auth/src/App.js
+++ frontend/frontend-auth/src/App.js
@@ -2,16 +2,18 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 
 export default function App() {
   return (
     <BrowserRouter>
       <nav>
-        <Link to="/signup">Đăng ký</Link> | <Link to="/login">Đăng nhập</Link>
+        <Link to="/signup">Đăng ký</Link> | <Link to="/login">Đăng nhập</Link> | <Link to="/profile">Hồ sơ cá nhân</Link>
       </nav>
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/frontend-auth/src/services/api.js
+++ frontend/frontend-auth/src/services/api.js
@@ -4,5 +4,17 @@ const API = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
 
+// ========== AUTH ==========
 export const signup = (data) => API.post("/auth/signup", data);
 export const login = (data) => API.post("/auth/login", data);
+
+// ========== PROFILE ==========
+export const getProfile = async (token) =>
+  API.get("/profile", {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+export const updateProfile = async (token, data) =>
+  API.put("/profile", data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
